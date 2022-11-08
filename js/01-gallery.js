154d4e395be1b7d7966cf67a0fdafa1ec9e0ea71
Vladislav Mararenko <mararenko.vladislav@gmail.com>
2022-11-08 15:04:04 +0200

    HW 7 draft

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const instance = basicLightbox.create("");
+
+const refs = {
+  galleryList: document.querySelector(".gallery"),
+  modal: instance.element(),
+};
+
+const makeGalleryMarkup = () => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+    })
+    .join("");
+};
+
+const render = () => {
+  refs.galleryList.innerHTML = makeGalleryMarkup();
+};
+
+const onEscmodalClose = (e) => {
+  if (e.keyCode === 27) {
+    instance.close();
+  }
+};
+
+const handleClick = (e) => {
+  e.preventDefault();
+
+  if (e.target === e.currentTarget) {
+    return;
+  }
+
+  refs.modal.innerHTML = `<img
+        src="${e.target.dataset.source}"
+        alt="${e.target.alt}"/>`;
+
+  instance.show();
+};
+
+render();
+
+refs.galleryList.addEventListener("click", handleClick);
+refs.galleryList.addEventListener("keydown", onEscmodalClose);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryList: document.querySelector(".gallery"),
+};
+
+const makeGalleryMarkup = () => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+    })
+    .join("");
+};
+
+const render = () => {
+  refs.galleryList.innerHTML = makeGalleryMarkup();
+};
+
+render();
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
